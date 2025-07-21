cf8e8726ff44407595ec9c38fe1db4eb9ae224ad
m-dhieu <m.dhieu@alustudent.com>
2025-07-21 21:45:27 +0200

    Puppet script to configure ssh client to connect to a server without a password.

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,13 @@
-# connect to a server without a password
-# set up client config file
-
-file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  match   => '^\s*PasswordAuthentication\s+',
-  replace => true,
-}
+# connect to a server without a password and set up client config file
 
 file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^\s*IdentityFile\s+',
-  replace => true,
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
+}
+
+file_line { 'Turn off password auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
